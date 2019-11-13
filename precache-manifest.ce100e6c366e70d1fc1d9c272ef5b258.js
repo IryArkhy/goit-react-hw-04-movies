a3b8b984e31355dd2662467ddbcdfa25f5cc4f0f
IryArkhy <37odri@gmail.com>
2019-11-14 01:20:13 +0200

    Updates

---
+++ precache-manifest.ce100e6c366e70d1fc1d9c272ef5b258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323a9b2498d1f52d3b7b18ea3dec6dc6",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "1b9eeba243b389d23aa4",
+    "url": "/goit-react-hw-04-movies/static/css/main.3d9e0956.chunk.css"
+  },
+  {
+    "revision": "deb6d41eb5cc4ccd9034",
+    "url": "/goit-react-hw-04-movies/static/js/2.46329c64.chunk.js"
+  },
+  {
+    "revision": "1b9eeba243b389d23aa4",
+    "url": "/goit-react-hw-04-movies/static/js/main.af0241f9.chunk.js"
+  },
+  {
+    "revision": "1464c0939378c4dfacbb",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.11f26c4b.js"
+  }
+]);
